3826baaee7da000216d0c48eb0478494d183c030
ruandqm <ruandqm@gmail.com>
2023-05-05 08:07:19 -0300

    add cors middeware

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ from fastapi import Depends, FastAPI
 from pydantic import BaseModel
 from middlewares.auth import auth
 from controllers import ocrGoogleVision, extractData
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 
 class reqModel(BaseModel):
     image: str
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
